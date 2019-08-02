5e2b5fb9db96a981608b3127a4c25cb2de166da6
kutataku <gagakki.sin@gmail.com>
2019-08-02 13:32:02 +0900

    second

---
+++ WebProj4/src/com/internous/webproj4/action/InquiryCompleteAction.java
@@ -0,0 +1,47 @@
+package com.internous.webproj4.action;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.internous.webproj4.dao.InquirycompleteDAO;
+import com.internous.webproj4.dto.InquiryDTO;
+import com.opensymphony.xwork2.ActionSupport;
+public class InquiryCompleteAction extends ActionSupport{
+     private String name;
+     private String qtype;
+     private String body;
+private List<InquiryDTO> inquiryDTOList = new ArrayList<InquiryDTO>();
+     public String execute() {
+          String ret=ERROR;
+InquirycompleteDAO dao=new InquirycompleteDAO();
+int count= dao.insert(name, qtype, body);
+if(count > 0){
+inquiryDTOList=dao.select();
+          ret=SUCCESS;
+          }
+          return ret;
+}
+     public String getName() {
+          return name;
+     }
+          public void setName(String name) {
+        	  this.name = name;
+          }
+
+          public String getQtype() {
+        	  return qtype;
+          }
+          public void setQtype(String qtype) {
+        	  this.qtype = qtype;
+          }
+               public String getBody() {
+                    return body;
+          }
+          public void setBody(String body) {
+        	  this.body = body;
+          }
+          public List<InquiryDTO> getInquiryDTOList() {
+        	  return inquiryDTOList;
+          }
+          public void setInquiryDTOList(List<InquiryDTO> inquiryDTOList)
+          { this.inquiryDTOList = inquiryDTOList;
+          } }
---
+++ WebProj4/src/com/internous/webproj4/action/WelcomeAction.java
@@ -0,0 +1,9 @@
+package com.internous.webproj4.action;
+import com.opensymphony.xwork2.ActionSupport;
+
+
+public class WelcomeAction extends ActionSupport {
+	public String execute() {
+		return SUCCESS; }
+
+}
